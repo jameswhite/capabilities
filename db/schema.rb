9149213c72bb2ad7178e66987a26aca49b94c747
Rick Bradley <rick@rickbradley.com>
2019-05-30 21:01:57 -0500

    Add applications table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_31_015959) do
+ActiveRecord::Schema.define(version: 2019_05_31_020134) do
+
+  create_table "applications", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "architectures", force: :cascade do |t|
     t.string "name"
