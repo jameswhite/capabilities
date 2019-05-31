bc669b10b61bf3b239f94945542d893f22d30b5c
Rick Bradley <rick@rickbradley.com>
2019-05-30 20:57:59 -0500

    Add operating systems table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_31_015552) do
+ActiveRecord::Schema.define(version: 2019_05_31_015706) do
 
   create_table "architectures", force: :cascade do |t|
     t.string "name"
@@ -31,6 +31,12 @@ ActiveRecord::Schema.define(version: 2019_05_31_015552) do
   end
 
   create_table "host_systems", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "operating_systems", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
