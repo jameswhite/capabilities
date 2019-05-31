86886071e22d9759ed089704042b3de69b4d914c
Rick Bradley <rick@rickbradley.com>
2019-05-30 21:00:27 -0500

    Add languages table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_31_015706) do
+ActiveRecord::Schema.define(version: 2019_05_31_015959) do
 
   create_table "architectures", force: :cascade do |t|
     t.string "name"
@@ -31,6 +31,12 @@ ActiveRecord::Schema.define(version: 2019_05_31_015706) do
   end
 
   create_table "host_systems", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "languages", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
